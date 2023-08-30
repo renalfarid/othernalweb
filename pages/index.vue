f2d976af472fc83e94b5f8ed1c30b427e4daefa2
Nal Farid <enalfarid@Nals-MacBook-Air.local>
2023-08-30 17:44:05 +0800

    api call

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <h1>Users</h1>
+      
+      <ul>
+        <li>{{ users.data.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  
+  const users = ref([]);
+  
+  onMounted(async () => {
+    try {
+      const response = await axios.get('https://api.agify.io/?name=enal');
+      users.value = response;
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  });
+  </script>
+  
